4e96a6be7a8874dc3329e478bc0df8b756472a0d
addy-steady-go <adhiraj.ss04@gmail.com>
2026-09-02 16:08:38 +0530

    Operators in C++

---
+++ Operators.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 5;
+	int b = 10;
+	
+	//Operators in C++
+	//1- Arithmetic Operators
+	
+	cout<<a+b<<endl;
+	cout<<a-b<<endl;
+	cout<<a*b<<endl;
+	cout<<a/b<<endl;
+	cout<<a%b<<endl;
+	cout<<++a<<" "<<++b<<endl;
+	cout<<a++<<" "<<b++<<endl;
+	cout<<--a<<" "<<--b<<endl;
+	cout<<a--<<" "<<b--<<endl;
+	cout<<a<<" "<<b<<endl;
+	//2-Assignment Operators '='
+	
+	//3-Comparison Operators
+	
+	cout<<(a==b)<<endl;
+	cout<<(a!=b)<<endl;
+	cout<<(a>b)<<endl;
+	cout<<(a<b)<<endl;
+	cout<<(a>=b)<<endl;
+	cout<<(a<=b)<<endl;
+	
+	//4-Logical Operators
+	// &&(Logical AND) , ||(Logical OR) , !(Logical NOT)
+	return 0;
+}
